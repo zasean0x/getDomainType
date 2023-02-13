8c24b8f897b1027e30de44dbcf1d8038116eef8f
zasean0x <seanmulder@protonmail.com>
2023-02-13 17:17:52 +0200

    not how arrays work

--- getDomainType-args.sh
+++ getDomainType-args.sh
@@ -2,7 +2,7 @@
 # Argument Edition
 echo "./getDomainType-args.sh (username) (api key) (url)"
 echo "./getDomainType-args.sh sean r43rjuht43t8338 https://example.com
-username=$1
-apiKey=$2
-url=$3
+username=$0
+apiKey=$1
+url=$2
 curl -H "Authorization: whm $username:$apiKey" "$url:2087/json-api/get_domain_info?api.version=1" | jq '.data.domains[] | "\(.parent_domain) \(.domain_type)"'
