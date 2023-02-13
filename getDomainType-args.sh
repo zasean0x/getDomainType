c573aaad6509bb35a02663f660687b2fb9e1050c
zasean0x <seanmulder@protonmail.com>
2023-02-13 17:11:22 +0200

    Create getDomainType-args.sh

---
+++ getDomainType-args.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Argument Edition
+echo "./getDomainType-args.sh (username) (api key) (url)"
+echo "./getDomainType-args.sh sean r43rjuht43t8338 https://example.com
+username=$1
+apiKey=$2
+url=$3
+curl -H "Authorization: whm $username:$apiKey" "$url:2087/json-api/get_domain_info?api.version=1" | jq '.data.domains[] | "\(.parent_domain) \(.domain_type)"'
